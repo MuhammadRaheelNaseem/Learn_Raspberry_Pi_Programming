8fd8ca3e50eb110fe17d766e35f1969af20532fa
Muhammad Raheel <63813881+MuhammadRaheelNaseem@users.noreply.github.com>
2022-06-14 15:21:46 +0500

    Delete DistanceSensor/DistanceSensor directory

--- DistanceSensor/DistanceSensor/dis2.py
+++
@@ -1,6 +0,0 @@
-from gpiozero import DistanceSensor
-
-dis = DistanceSensor(echo=17, trigger=4)
-
-while True:
-    print("%.3f"%dis.distance)
--- DistanceSensor/DistanceSensor/dis3.py
+++
@@ -1,9 +0,0 @@
-from gpiozero import DistanceSensor
-
-dis = DistanceSensor(echo=17, trigger=4)
-
-while True:
-    ultrasonic.wait_for_in_range()
-    print("In range")
-    ultrasonic.wait_for_out_of_range()
-    print("Out of range")
