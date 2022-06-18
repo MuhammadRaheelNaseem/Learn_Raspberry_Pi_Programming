5127738d442c9d7319b9cceb492c8f811655a740
Muhammad Raheel <63813881+MuhammadRaheelNaseem@users.noreply.github.com>
2022-06-18 19:42:26 +0500

    Create dht2.py

---
+++ TemperatureSensor/dht2.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import dht11
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 14
+instance = dht11.DHT11(pin = 14)
+# read() method, which will return DHT11 Result object with actual values and error code.
+result = instance.read()
+
+if result.is_valid():
+    print("Temperature: %-3.1f C" % result.temperature)
+    print("Humidity: %-3.1f %%" % result.humidity)
+else:
+    print("Error: %d" % result.error_code)
