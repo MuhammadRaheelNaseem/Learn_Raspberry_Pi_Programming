2ea0a8d87a479d9d8317ec53d3d6a2ce9b048718
Muhammad Raheel <63813881+MuhammadRaheelNaseem@users.noreply.github.com>
2022-06-18 20:35:23 +0500

    Create camera4.py

---
+++ Picamera/camera4.py
@@ -0,0 +1,9 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+camera.start_preview()
+camera.start_recording('/home/pi/Desktop/video.h264')
+sleep(5)
+camera.stop_recording()
+camera.stop_preview()
