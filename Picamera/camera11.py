7e4129ac03378b6ebf2b1b15f941bd7e541deaf6
Muhammad Raheel <63813881+MuhammadRaheelNaseem@users.noreply.github.com>
2022-06-18 20:41:17 +0500

    Create camera11.py

---
+++ Picamera/camera11.py
@@ -0,0 +1,10 @@
+from picamera import PiCamera, Color
+
+camera = PiCamera()
+
+camera.start_preview()
+for i in range(100):
+    camera.annotate_text = "Contrast: %s" % i
+    camera.contrast = i
+    sleep(0.1)
+camera.stop_preview()
